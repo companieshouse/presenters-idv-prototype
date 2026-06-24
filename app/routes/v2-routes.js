f2677d8d005b6a54574ac8c2028f259dc37963f9
Andreea-Popescu99 <apopescu@companieshouse.gov.uk>
2026-06-24 20:10:17 +0100

    Add CHS confirm-presenter-statements view and route
    Add a new Nunjucks template app/views/V2/chs/confirm-presenter-statements.html that renders a single confirmation page for CHS presenters, selecting heading and checkbox items based on the session's presenter type. Simplify the presenter-type-radio POST handler in app/routes/v2-routes.js to redirect to /V2/chs/confirm-presenter-statements (preserving the 'none' => /V2/cannot-file case) instead of multiple per-presenter redirects. Also update the presenter-type-radio form action to /V2/presenter-type-radio.

--- app/routes/v2-routes.js
+++ app/routes/v2-routes.js
@@ -12,7 +12,6 @@ router.post('/V2/chs/check-verification', (req, res) => {
   }
 })
 
-
 router.post('/v2/chs/check-verification', (req, res) => {
   const haveVerified = req.body.haveVerified
 
@@ -29,36 +28,12 @@ router.post('/V2/presenter-type-radio', function(req, res) {
 
   const presenter = req.session.data['what-type-of-presenter']
 
-  if (presenter === 'acsp') {
-    return res.redirect('/V2/confirm-acsp-statements')
-  }
-
-  if (presenter === 'acspEmployee') {
-    return res.redirect('/V2/confirm-acsp-employee-statements')
-  }
-
-  if (
-    presenter === 'director' ||
-    presenter === 'employeeCompany' ||
-    presenter === 'employeeCorporate'
-  ) {
-    return res.redirect('/V2/confirm-officer-statements')
-  }
-
-  if (presenter === 'externalPresenter') {
-    return res.redirect('/V2/confirm-external-presenter-statements')
-  }
-
-  if (presenter === 'insolvencyPractitioner') {
-    return res.redirect('/V2/confirm-insolvency-practitioner-statements')
-  }
-
   if (presenter === 'none') {
     return res.redirect('/V2/cannot-file')
   }
 
-  res.redirect('/V2/what-type-of-presenter')
-})
+  return res.redirect('/V2/chs/confirm-presenter-statements')
 
+})
 
 module.exports = router
